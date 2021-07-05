ceed14a29f6087b94c687356cf5f4b28c86c53dc
Luis Guisado <lguisadom@gmail.com>
2021-07-05 02:12:30 -0500

    Anotación @FunctionalInterface

--- src/com/lagm/interfaz/Operacion.java
+++ src/com/lagm/interfaz/Operacion.java
@@ -1,6 +1,7 @@
 package com.lagm.interfaz;
 
 // Una interfaz funcional es aquella que define una única operación (un solo método)
+@FunctionalInterface // Anotación java 8 para definir interfaces funcionales
 public interface Operacion {
 	double calcular(double n1, double n2);
 }
